a56e2adbaaaebb0e90ed4f117a58013eb6be3bdd
TessyJames28 <tessyjames28@gmail.com>
2023-09-18 03:02:54 +0100

    Modified serializers file

--- SchoolMatch/user/serializers.py
+++ SchoolMatch/user/serializers.py
@@ -1,14 +1,31 @@
-from .models import CustomUser, Favorite 
+from .models import Favorite 
 from rest_framework import serializers
+from django.contrib.auth.models import User 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
     
     class Meta:
-        model = CustomUser
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'confirm_password']
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'confirm_password']
 
+    def create(self, validated_data):
+        # extra and remove the 'password' field from the validated data
+        password = validated_data.pop('password')
+        confirm_password = validated_data.pop('confirm_password', None)
+        
+        if 'password' in validated_data and confirm_password != validated_data['password']:
+            raise serializers.ValidationError("Password do not match")
+        
+        # create the user with the remaining validated data
+        user = User.objects.create_user(**validated_data)
+        
+        # set user's password separately
+        user.set_password(password)
+        user.save()
+        
+        return user
 
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
