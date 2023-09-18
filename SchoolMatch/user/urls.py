110256232f7161ef2d3c0b5ea0d5ec0374944b19
TessyJames28 <tessyjames28@gmail.com>
2023-09-18 03:01:49 +0100

    api endpoints

--- SchoolMatch/user/urls.py
+++ SchoolMatch/user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('create-account', views.CustomUserView.as_view()),
+    path('create-account', views.user_view, name="user_account"),
     path('favorites', views.FavoriteView.as_view()),
+    path('login/', obtain_auth_token, name="login"),
+    path('profile', views.user_profile, name="user_profile"),
+    path('logout/', views.logout),
         
 ]
