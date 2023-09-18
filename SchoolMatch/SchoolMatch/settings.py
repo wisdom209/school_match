1215e26b71093006f4388a63e2b64b4bd2a76c4e
TessyJames28 <tessyjames28@gmail.com>
2023-09-18 03:02:08 +0100

    Settings file

--- SchoolMatch/SchoolMatch/settings.py
+++ SchoolMatch/SchoolMatch/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'school',
     'user',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -76,26 +77,26 @@ WSGI_APPLICATION = 'SchoolMatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SchoolMatch',
-        'USER': 'school_match_user',
-        'PASSWORD': 'root#1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {   
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'SchoolMatch',
+#         'USER': 'school_match_user',
+#         'PASSWORD': 'root#1234',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {   
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
