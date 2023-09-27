d039aa93cbaaa3b99a6c1c438fc869bbed2972c9
TessyJames28 <tessyjames28@gmail.com>
2023-09-27 12:02:50 +0100

    Added an endpoint to view single school/program

--- SchoolMatch/school/urls.py
+++ SchoolMatch/school/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('school', views.SchoolView.as_view()),
     path('department', views.DepartmentView.as_view()),
     path('search', views.SearchViewSet.as_view({'get':'list'})),
+    path('program/<int:id>', views.SingleDepartmentView.as_view()),
     
 ]
