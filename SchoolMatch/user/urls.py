d1b61cc12d398ec569cec65a7ea8dbd03b94b753
TessyJames28 <tessyjames28@gmail.com>
2023-10-06 09:10:38 +0100

    Improve the add favorite endpoint

--- SchoolMatch/user/urls.py
+++ SchoolMatch/user/urls.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('create-account', views.user_view, name="user_account"),
-    path('favorite/<int:user_id>', views.FavoriteView.as_view()),
+    path('favorite', views.FavoriteView.as_view()),
     path('favorite/<int:user_id>/<int:id>', views.RetrieveDeleteFavorite.as_view()),
     path('login/', obtain_auth_token, name="login"),
     path('profile', views.user_profile, name="user_profile"),
